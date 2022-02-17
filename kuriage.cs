02ee9216e18d039e3d234ae20aabcbf1164e30ed
Jc21-190118 <75291144+Jc21-190118@users.noreply.github.com>
2022-02-17 14:03:41 +0900

    メソッド名再変更

--- kuriage.cs
+++ kuriage.cs
@@ -8,7 +8,7 @@ namespace WindowsFormsApp1
 {
     public class kuriage
     {
-        public static int uriage10(int j)
+        public static int kuriage10(int j)
         {
             if (j == 0)
             {
